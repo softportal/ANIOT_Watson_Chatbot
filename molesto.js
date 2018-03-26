bedfcf59aac744d9bf6cad26a6e9fc4a421aadca
pilotAlpal <lsegar01@ucm.es>
2018-03-26 21:34:27 +0200

    basic system, bugged

--- molesto.js
+++ molesto.js
@@ -6,7 +6,20 @@ var yamlinfo;
 var ConversationV1 = require('watson-developer-cloud/conversation/v1');
 
 config = yaml.sync('credentials.yaml')
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
+MongoClient.connect(url, function(err, db) {
+	  if (err) throw err;
+	  var dbo = db.db("test");
+	  var query = {};
+	  dbo.collection("restaurants").find(query).toArray(function(err, result) {
+		      if (err) throw err;
+		      console.log(result);
+		      console.log('pene');
+		      db.close();
+		    });
+});
 
 console.log(config["Username"])
 
@@ -18,12 +31,20 @@ var conversation = new ConversationV1({
 });
 
 var workspace_id = config["Workspace_ID"]; // replace with workspace ID
+//var core = new targetData();
+
 
 // Start conversation with empty message.
 conversation.message({
   workspace_id: workspace_id
   }, processResponse);
-
+function targetData(){ };
+targetData.prototype.set_foodType= function(foodType) { this.foodType=foodType; };
+targetData.prototype.set_price= function(price) { this.price=price; };
+targetData.prototype.set_location= function(place) { this.place=place; };
+targetData.prototype.check_values=function(){
+    return ((this.price != undefined) && (this.place != undefined) && (this.foodType != undefined));
+};
 // Process the conversation response.
 function processResponse(err, response) {
   if (err) {
@@ -36,18 +57,28 @@ function processResponse(err, response) {
     console.log('Detected intent: #' + response.intents[0].intent);
   }
   //if a variable..
-  if (response.context.hasOwnProperty('targetLocation')) 
+  if (response.context.hasOwnProperty('targetLocation')){ 
     console.log('Target Location: #' + response.context.targetLocation);
   //i
-  if (response.context.hasOwnProperty('targetPrice'))
-    console.log('Target Price:#' + response.context.tagetPrice);	
+    //core.set_location(response.context.targetLocation);
+  }
+  //if (response.context.hasOwnProperty('targetFoodType')){
+  //  console.log('Target Foodtype:#' + response.context.targetFoodType);	
+  //// Display the output from dialog, if any.
+  //  //core.set_foodType(response.context.targetFoodType);
+  //}
+  if (response.context.hasOwnProperty('targetPrice')){
+    console.log('Target Price:#' + response.context.targetPrice);	
   // Display the output from dialog, if any.
+    //core.set_price(response.context.targetPrice);
+  }
   if (response.output.text.length != 0) {
       console.log(response.output.text[0]);
   }
 
-  // Prompt for the next round of input.
-    var newMessageFromUser = prompt('>> ');
+  //if (core.check_values()){console.log('FILLED');};
+  //if (!core.check_values()){console.log('UNFILLED');};
+  // Prompt for the next round of input.  var newMessageFromUser = prompt('>> ');
     // Send back the context to maintain state.
     conversation.message({
       workspace_id: workspace_id,
